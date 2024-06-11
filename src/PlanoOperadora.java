198cd60148402b77eb4680568c454148e6ededa3
Ramon Marques <ramonlmarques@hotmail.com>
2024-06-11 16:10:04 -0300

    atualizando as classes criadas

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,17 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano){
+            case "T":{
+                System.out.println("5gb Youtube");
+            }
+            case "M":{
+                System.out.println("WhatsApp e Instagram Ilimitado");
+            }
+            case "B":{
+                System.out.println("100min de Ligações");
+            }
+        }
+    }
+}
